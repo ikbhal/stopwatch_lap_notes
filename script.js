42625349b2919a7a25bbec8309e0273880ad7f87
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-11 09:15:03 +0530

    lap, resume, pause, reset, lap butotn, table

--- script.js
+++ script.js
@@ -1,65 +1,76 @@
-const startBtn = document.getElementById('startBtn');
-const stopBtn = document.getElementById('stopBtn');
-const resetBtn = document.getElementById('resetBtn');
-const lapBtn = document.getElementById('lapBtn');
-const timeDisplay = document.getElementById('timeDisplay');
-const lapTable = document.getElementById('lapTable');
-let startTime;
-let interval;
-let lapNumber = 1;
+$(document).ready(function() {
+  const startBtn = $('#startBtn');
+  const pauseBtn = $('#pauseBtn');
+  const resumeBtn = $('#resumeBtn');
+  const resetBtn = $('#resetBtn');
+  const lapBtn = $('#lapBtn');
+  const timeDisplay = $('#timeDisplay');
+  const lapTable = $('#lapTable');
+  let startTime;
+  let interval;
+  let lapNumber = 1;
 
-startBtn.addEventListener('click', startTimer);
-stopBtn.addEventListener('click', stopTimer);
-resetBtn.addEventListener('click', resetTimer);
-lapBtn.addEventListener('click', lapTimer);
+  startBtn.click(startTimer);
+  pauseBtn.click(pauseTimer);
+  resumeBtn.click(resumeTimer);
+  resetBtn.click(resetTimer);
+  lapBtn.click(lapTimer);
 
-function startTimer() {
-  startTime = Date.now() - (startTime ? startTime : 0);
-  interval = setInterval(updateTime, 10);
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
-}
+  function startTimer() {
+    startTime = Date.now() - (startTime ? startTime : 0);
+    interval = setInterval(updateTime, 10);
+    startBtn.prop('disabled', true);
+    pauseBtn.prop('disabled', false);
+  }
 
-function stopTimer() {
-  clearInterval(interval);
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
-}
+  function pauseTimer() {
+    clearInterval(interval);
+    startBtn.prop('disabled', true);
+    pauseBtn.prop('disabled', true);
+    resumeBtn.prop('disabled', false);
+  }
 
-function resetTimer() {
-  clearInterval(interval);
-  startTime = undefined;
-  lapNumber = 1;
-  timeDisplay.textContent = '00:00:00';
-  lapTable.innerHTML = '';
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
-}
+  function resumeTimer() {
+    interval = setInterval(updateTime, 10);
+    startBtn.prop('disabled', true);
+    pauseBtn.prop('disabled', false);
+    resumeBtn.prop('disabled', true);
+  }
 
-function lapTimer() {
-  const lapTime = calculateTime();
-  const newRow = document.createElement('div');
-  newRow.classList.add('lap-row');
-  newRow.innerHTML = `Lap ${lapNumber}: <span class="lap-time">${lapTime}</span>`;
-  lapTable.appendChild(newRow);
-  lapNumber++;
-}
+  function resetTimer() {
+    clearInterval(interval);
+    startTime = undefined;
+    lapNumber = 1;
+    timeDisplay.html('00:00:00');
+    lapTable.html('');
+    startBtn.prop('disabled', false);
+    pauseBtn.prop('disabled', true);
+    resumeBtn.prop('disabled', true);
+  }
 
-function updateTime() {
-  const time = calculateTime();
-  timeDisplay.innerHTML = time;
-}
+  function lapTimer() {
+    const lapTime = calculateTime();
+    const newRow = $('<div>').addClass('lap-row').html(`Lap ${lapNumber}: <span class="lap-time">${lapTime}</span>`);
+    lapTable.append(newRow);
+    lapNumber++;
+  }
 
-function calculateTime() {
-  const currentTime = Date.now() - startTime;
-  const milliseconds = Math.floor((currentTime % 1000) / 10);
-  const seconds = Math.floor((currentTime / 1000) % 60);
-  const minutes = Math.floor((currentTime / 1000 / 60) % 60);
-  const hours = Math.floor(currentTime / 1000 / 60 / 60);
-  
-  return `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)} <span class="milliseconds">${formatTime(milliseconds)}</span>`;
-}
+  function updateTime() {
+    const time = calculateTime();
+    timeDisplay.html(time);
+  }
 
-function formatTime(time) {
-  return time.toString().padStart(2, '0');
-}
+  function calculateTime() {
+    const currentTime = Date.now() - startTime;
+    const milliseconds = Math.floor((currentTime % 1000) / 10);
+    const seconds = Math.floor((currentTime / 1000) % 60);
+    const minutes = Math.floor((currentTime / 1000 / 60) % 60);
+    const hours = Math.floor(currentTime / 1000 / 60 / 60);
+
+    return `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)} <span class="milliseconds">${formatTime(milliseconds)}</span>`;
+  }
+
+  function formatTime(time) {
+    return time.toString().padStart(2, '0');
+  }
+});
